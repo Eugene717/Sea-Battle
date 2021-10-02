6eadcdc19004fabb2586e3a3010314b92ca5da87
Евгений <nowhere>
2021-10-02 13:15:50 +0300

    новые константы

--- Sea Battle/Constants.h
+++ Sea Battle/Constants.h
@@ -8,8 +8,12 @@ const int COL = 10;
 const char EMPTY = '\0';       //пусто
 const char DEAD = 'X';         //помер
 const char ALIVE = '\1';       //живой корабль
-const char ENEMY_ALIVE = ' ';  //живой корабль противника, что бы он не отображался на карте
+const char ENEMY_ALIVE = ' ';  //живой корабль противника
 const char MISS = '*';         //промах
+const int MIN_F_BOARD_X = 50;
+const int MIN_S_BOARD_X = 450;
+const int MIN_Y = 80;
+const int SQUARE_SIDE_SIZE = 30;
 
 static std::random_device RD;
 
