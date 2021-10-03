d9888ab26f5356c7bfd792805b868f4828c3dfe6
Евгений <nowhere>
2021-10-03 12:11:55 +0300

    новая система выстрела

--- Sea Battle/Human.cpp
+++ Sea Battle/Human.cpp
@@ -4,25 +4,40 @@
 Human::Human(const char& player) :Player(player)
 { }
 
-bool Human::Shoot(char(&enemy)[ROW][COL])
+bool Human::Shoot(char(&enemy)[ROW][COL], sf::RenderWindow& window)
 {
 	Point shot;
-	do
+	while (window.isOpen())
 	{
-		std::cin >> shot;
-		if (enemy[shot.GetY()][shot.GetX()] == MISS || enemy[shot.GetY()][shot.GetX()] == DEAD)
+		sf::Event event;
+		while (window.pollEvent(event))
 		{
-			std::cout << "Вы уже стреляли в эту ячейку!\n";
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+			}
+			if (event.type == event.MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
+			{
+				if (sf::IntRect(MIN_S_BOARD_X, MIN_Y, SQUARE_SIDE_SIZE * 10, SQUARE_SIDE_SIZE * 10).contains(sf::Mouse::getPosition(window)))
+				{
+					sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+					shot.SetValues((mousePos.y - MIN_Y) / 30, (mousePos.x - MIN_S_BOARD_X) / 30);
+
+					if (enemy[shot.GetY()][shot.GetX()] == ENEMY_ALIVE)
+					{
+						enemy[shot.GetY()][shot.GetX()] = DEAD;
+						return true;
+					}
+					else if (enemy[shot.GetY()][shot.GetX()] == EMPTY)
+					{
+						enemy[shot.GetY()][shot.GetX()] = MISS;
+						return false;
+					}
+				}
+			}
 		}
-		else if (enemy[shot.GetY()][shot.GetX()] == ENEMY_ALIVE)
-		{
-			enemy[shot.GetY()][shot.GetX()] = DEAD;
-			return true;
-		}
-		else
-		{
-			enemy[shot.GetY()][shot.GetX()] = MISS;
-			return false;
-		}
-	} while (true);
+	}
 }
